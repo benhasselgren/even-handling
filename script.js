ac3aa47c74bc41e59e3d08f3d66c31a897cd1512
Ben Hasselgren <ben.nadalito@gmail.com>
2018-03-24 16:28:10 +0000

    Addd bg color on click to streams

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ $(document).ready(function() {
 		$("button").first().attr("id", "one");
 	});
 	$("button").on("click", function() {
-		$(this).siblings("p").slideToggle(500);
+		$(this).siblings("p").slideToggle(500)
 	});
 	$("button").mouseover(function() {
 		$(this).fadeTo("medium", 0.5);
 	});
 	$("button").mouseleave(function() {
 		$(this).fadeTo("medium", 1);
+	});
+	$("#stream1_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+		$(".stream1").addClass('highlight_stream');
 	});
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
